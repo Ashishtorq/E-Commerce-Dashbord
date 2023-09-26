2ce3fc9c3dd74cf9c6f8c9853cec1ffb5428761d
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-27 03:40:58 +0530

    navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-
+import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Nav from "./Nav";
 function App() {
   return (
     <>
       <div className="App"><h1>Its working</h1>
-      
+      <Nav></Nav>
       </div>
     </>
   );
