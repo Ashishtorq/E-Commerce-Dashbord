92468c92a425b41e5f135b092e37332db84314ae
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-27 03:38:27 +0530

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <h1>Its working</h1>
+      <div className="App"><h1>Its working</h1>
+      
+      </div>
     </>
   );
 }
