e875f39b3e20039825b24ff22351c121cad10265
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-28 20:18:16 +0530

    Product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Fragment } from "react";
 import "./App.css";
-import Nav from "./Nav";
+import Nav from "./Components/Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <Fragment>
@@ -10,6 +11,7 @@ function App() {
           <Nav></Nav>
           <Routes>
           <Route path="/" element={<h1>Welcome Home !!!</h1>}></Route>
+          <Route path="/product" element={<h1>Get New Products here</h1>}></Route>
           <Route path="/add-product" element={<h1>Add Products</h1>}></Route>
           <Route path="/update-product" element={<h1>Product Listing update</h1>}></Route>
           <Route path="/logout" element={<h1>You are logged out !!</h1>}></Route>
